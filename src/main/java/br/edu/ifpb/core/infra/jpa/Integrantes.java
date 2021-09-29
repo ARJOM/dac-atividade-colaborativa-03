5e0dcd57f26374eaad74ed7bbb859ffee89b9662
Antônio Ricart <ricarteusuo11@gmail.com>
2021-09-29 10:03:03 -0300

    feat: adiciona persistencia de banco e implementa buscas foi implementado todas as buscas solicitadas nos requisitos da atividade

--- src/main/java/br/edu/ifpb/core/infra/jpa/Integrantes.java
+++ src/main/java/br/edu/ifpb/core/infra/jpa/Integrantes.java
@@ -5,6 +5,7 @@ import br.edu.ifpb.core.domain.Integrante;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Stateless
@@ -19,6 +20,19 @@ public class Integrantes {
     }
 
     public List<Integrante> todos(){
-        return manager.createQuery("FROM Integrante i", Integrante.class).getResultList();
+        return manager.createQuery("SELECT i FROM Integrante i", Integrante.class).getResultList();
+    }
+
+    public Integrante buscarPorCPF(String cpf){
+        Query query = manager.createQuery("SELECT i FROM Integrante i WHERE i.cpf=:cpf", Integrante.class);
+        query.setParameter("cpf", cpf);
+        return (Integrante) query.getSingleResult();
+    }
+
+    public List<Integrante> integrantesPorIntervaloEspecifico(){
+        return manager.createQuery(
+                "SELECT i FROM Integrante i WHERE i.dataDeNascimento>='20000101' AND i.dataDeNascimento<='20160420'",
+                Integrante.class
+        ).getResultList();
     }
 }
