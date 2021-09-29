69840eac72b2f1409da8ec82afb6acffae4711ea
Antônio Ricart <ricarteusuo11@gmail.com>
2021-09-29 11:25:40 -0300

    feat: implementa listagem de banda por local de origem

--- src/main/java/br/edu/ifpb/core/web/ControladorDeBandas.java
+++ src/main/java/br/edu/ifpb/core/web/ControladorDeBandas.java
@@ -3,20 +3,39 @@ package br.edu.ifpb.core.web;
 import br.edu.ifpb.core.domain.Banda;
 import br.edu.ifpb.core.infra.jpa.Bandas;
 
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
 
 @Named
-@RequestScoped
+@SessionScoped
 public class ControladorDeBandas {
 
     @Inject
     private Bandas bandas;
 
+    private String localDeOrigem = "";
+    private List<Banda> bandaList;
+
     public List<Banda> todasAsBandas(){
         return this.bandas.todos();
     }
 
+    public String buscarPorLocalDeOrigem(){
+        this.bandaList = bandas.buscaPorLocalDeOrigem(localDeOrigem);
+        return null;
+    }
+
+    public List<Banda> resultadoDeBusca(){
+        return this.bandaList;
+    }
+
+    public String getLocalDeOrigem() {
+        return localDeOrigem;
+    }
+
+    public void setLocalDeOrigem(String localDeOrigem) {
+        this.localDeOrigem = localDeOrigem;
+    }
 }
